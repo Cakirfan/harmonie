0888e1081bd473c11a2096d57a7b97e4fe27703d
irfan <cakirfan.001@gmail.com>
2023-12-25 22:36:58 +0100

    aktualisiert

--- src/components/Gründung.jsx
+++ src/components/Gründung.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Gründung = () => {
   return (
     <section
-      className="bg-info-subtle w-75 mx-auto rounded-2 border border-5 border-primary opacity-75 mt-5"
+      className="bg-dark text-white w-75 mx-auto rounded-2 border border-5 border-primary mt-5"
     >
       <div className="container p-3 z-1">
-        <p className="uns mx-auto fs-5 text-center link-offset-3-hover">
+        <p className="unsmx-auto fs-5 text-center link-offset-3-hover">
           Das hormonie e.V. wurde 2023 von Engagierten der Hizmet Bewegung
           gegründet. Der Verein hat sich zunächst auf Angebote entsprechend der
           Interessen und Bedürfnisse der eigenen Mitglieder und deren Kinder
