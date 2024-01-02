f6b739ef7154e9da37fe8baa9a84a3da2de954f8
irfan <cakirfan.001@gmail.com>
2024-01-02 20:57:45 +0100

    text updated

--- src/pages/UberUns.jsx
+++ src/pages/UberUns.jsx
@@ -32,44 +32,52 @@ const UberUns = () => {
           <span className="text-success fw-semibold auto-input"></span>
         </h1>
         <p className="fs-5">
-          Das hormonie e.V. wurde 2023 von Engagierten der Hizmet Bewegung
-          gegründet. Der Verein hat sich zunächst auf Angebote entsprechend der
-          Interessen und Bedürfnisse der eigenen Mitglieder und deren Kinder
-          konzentriert.
+          Im Jahr 2023 wurde der Harmonie e.V. von engagierten ehrenamtlichen
+          Bildungsmitarbeitern ins Leben gerufen. Anfangs konzentrierte sich der
+          Verein auf Angebote, die den Interessen und Bedürfnissen seiner
+          Mitglieder und deren Kinder entsprachen.
         </p>
         <p className="fs-5">
-          Daher standen in der Gründungsphase vor allem die kulturelle und
-          religiöse Bildung im Fokus. Mittlerweile hat sich das Spektrum der
-          Tätigkeitsfelder des Dokuha entsprechend der Interessenvielfalt und
-          des Integrationsprozesses der Mitglieder_innen erweitert.
+          In der Gründungsphase lag der Fokus vor allem auf kultureller und
+          religiöser Bildung. Im Laufe der Zeit hat sich das Tätigkeitsspektrum
+          des Harmonie entsprechend der Vielfalt der Interessen und des
+          Integrationsprozesses der Mitglieder_innen erweitert.
         </p>
         <p className="fs-5">
-          Neben der Kultur und Religion fokussieren wir uns ebenso auf folgende
-          Themenbereiche:
+          Neben Kultur und Religion liegt unser Augenmerk auch auf folgenden
+          Themenbereichen:
         </p>
-        <p className="fs-5 fw-semibold">
-          Kinder und Jugendarbeit Dialog (Interkulturell, Interreligiös,
-          Intrareligiös) Lokale Projekte Empowerment von Frauen Empowerment von
-          Menschen mit Fluchterfahrung
+        <ul>
+          <li className="fs-5 fw-semibold">Kinder- und Jugendarbeit</li>
+          <li className="fs-5 fw-semibold">
+            Dialog (Interkulturell, Interreligiös, Intrareligiös)
+          </li>
+          <li className="fs-5 fw-semibold">Lokale Projekte</li>
+          <li className="fs-5 fw-semibold">Empowerment von Frauen</li>
+          <li className="fs-5 fw-semibold">
+            Empowerment von Menschen mit Fluchterfahrung
+          </li>
+        </ul>
+        <p className="fs-5">
+          Eine zentrale Aufgabe des Vereins besteht darin, Mitglieder_innen
+          weiterzubilden, ihre Teilhabe an der Gesellschaft zu fördern und ihr
+          soziales Engagement zu stärken.
         </p>
         <p className="fs-5">
-          Eine Hauptaufgabe des Vereins liegt darin Mitglieder_innen
-          weiterzubilden, Ihre Teilhabe an der Gesellschaft und Ihr soziales
-          Engagement zu fördern.
+          Das Harmonie leistet einen integrativen Beitrag zum nachhaltigen
+          Frieden in der Gesellschaft, indem es sich mit Gemeinschaften des
+          öffentlichen Rechts und anderen Gruppen, die sich auf das Grundgesetz
+          berufen, engagiert.
         </p>
         <p className="fs-5">
-          Das Dokuha leistet einen integrativen Beitrag zum nachhaltigen Frieden
-          in der Gesellschaft mit den Gemeinschaften des öffentlichen Rechts und
-          anderen Gruppen, welche sich auf das Grundgesetz berufen.
+          Wir setzen uns entschieden gegen jeglichen Extremismus und
+          Radikalismus ein. Darüber hinaus fördern wir gezielt Begegnungen
+          zwischen Menschen unterschiedlicher Generationen, Anschauungen,
+          Religionen und Nationalitäten.
         </p>
         <p className="fs-5">
-          Wir setzen Uns gegen jedweden Extremismus und Radikalismus ein und
-          fördern gezielt die Begegnungen zwischen Menschen unterschiedlicher
-          Generationen, Anschauungen, Religionen und Nationalitäten.
-        </p>
-        <p className="fs-5">
-          In den nachfolgenden Seiten finden sie nähere Informationen zu unseren
-          Tätigkeitsfeldern.
+          Auf den folgenden Seiten finden Sie detaillierte Informationen zu
+          unseren Tätigkeitsfeldern.
         </p>
       </div>
     </section>
