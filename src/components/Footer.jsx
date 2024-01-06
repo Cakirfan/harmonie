78bd568b5fb0869c5635f0c076691f5586d1aaa0
irfan <cakirfan.001@gmail.com>
2024-01-06 20:49:16 +0100

    path added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
   const themen = [
     {
       name: "Kultur und Kunst",
-      href: "/",
+      href: "kulturundkunst",
     },
     {
       name: "Religion",
@@ -83,8 +83,8 @@ const Footer = () => {
       style={{ marginTop: "5rem" }}
     >
       <div>
-        <div className="row d-flex align-content-center justify-content-center bg-dark mb-3 py-2">
-          <div className="col-6 text-end fs-2 mb-3 mt-4 mt-md-2">
+        <div className="row bg-dark mb-3 py-2">
+          <div className="col text-end fs-2 mb-3 mt-4 mt-md-2">
             Kontaktieren Sie uns per{" "}
             <FontAwesomeIcon
               className="fs-1 ms-3 text-end"
@@ -92,7 +92,7 @@ const Footer = () => {
               icon={faHandPointRight}
             />
           </div>
-          <div className="col-6 mt-4 mt-md-2 py-2">
+          <div className="col mt-4 mt-md-2 py-2">
             {sozialIcons.map((icon, i) => (
               <Link key={i} to={icon.href}>
                 <button
@@ -109,57 +109,55 @@ const Footer = () => {
           className="row bg-dark px-3 py-3 text-white text-center"
           style={{ marginTop: "-.96rem" }}
         >
-          <div className="row">
-            <div className="col lh-1">
-              <img className="rounded-2" src={logo} width={150} alt="" />
-              <p className="fs-4 mt-2">Harmonie e.V.</p>
-              <p className="fs-5">Zufuhrstraße 2, 41462 Neuss</p>
-              <p className="fs-5">info@harmonie.de</p>
-              <p className="fs-5">+49 (111) 11 11 11 11</p>
-            </div>
-            <div className="col text-start d-flex flex-column justify-content-center">
-              <div className="row mt-3 mt-md-4">
-                <div className="col d-flex gap-2 gap-md-5">
-                  <span>Aktuelles</span>
-                  <span>Archiv</span>
-                  <Link
-                    to="impressum"
-                    className="list-group-item text-decoration-none"
-                  >
-                    Impressum
-                  </Link>
-                  <span>Datenschutzerklärung</span>
-                </div>
-                <div className="col d-flex flex-wrap gap-md-5 flex-md-nowrap">
-                  <ul className="mt-4 mt-md-5 lh-2 text-decoration-none">
-                    <h2>Themen</h2>
-                    {themen.map((thema, i) => (
-                      <li key={i}>
-                        <Link
-                          to={thema.href}
-                          className="list-group-item text-decoration-none"
-                        >
-                          <FaArrowRight className="me-2" />
-                          {thema.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                  <ul className="mt-4 mt-md-5 lh-2 text-decoration-none">
-                    <h2>Projekte</h2>
-                    {projekte.map((projekt, i) => (
-                      <li key={i}>
-                        <Link
-                          to={projekt.href}
-                          className="list-group-item text-decoration-none"
-                        >
-                          <FaArrowRight className="me-2" />
-                          {projekt.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+          <div className="col lh-1">
+            <img className="rounded-2" src={logo} width={150} alt="" />
+            <p className="fs-4 mt-2">Harmonie e.V.</p>
+            <p className="fs-5">Zufuhrstraße 2, 41462 Neuss</p>
+            <p className="fs-5">info@harmonie.de</p>
+            <p className="fs-5">+49 (111) 11 11 11 11</p>
+          </div>
+          <div className="col text-start d-flex flex-column justify-content-center">
+            <div className="row mt-3 mt-md-4">
+              <div className="col-12 d-flex gap-2 gap-md-5">
+                <span>Aktuelles</span>
+                <span>Archiv</span>
+                <Link
+                  to="impressum"
+                  className="list-group-item text-decoration-none"
+                >
+                  Impressum
+                </Link>
+                <span>Datenschutzerklärung</span>
+              </div>
+              <div className="col-12 d-flex flex-wrap gap-md-5 flex-md-nowrap">
+                <ul className="mt-4 mt-md-5 lh-2 text-decoration-none">
+                  <h2>Themen</h2>
+                  {themen.map((thema, i) => (
+                    <li key={i}>
+                      <Link
+                        to={thema.href}
+                        className="list-group-item text-decoration-none"
+                      >
+                        <FaArrowRight className="me-2" />
+                        {thema.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+                <ul className="mt-4 mt-md-5 lh-2 text-decoration-none">
+                  <h2>Projekte</h2>
+                  {projekte.map((projekt, i) => (
+                    <li key={i}>
+                      <Link
+                        to={projekt.href}
+                        className="list-group-item text-decoration-none"
+                      >
+                        <FaArrowRight className="me-2" />
+                        {projekt.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
